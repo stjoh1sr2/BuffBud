7c5e523bd1368cba933f57fb07892132edb9741f
nickw00 <nickwright2010@gmail.com>
2021-04-19 19:14:37 -0400

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -5,23 +5,19 @@ import java.util.*;
 
 import javafx.stage.Stage;
 
-public class Utility extends GameWindow {
+public class Utility {
+	private static File file = new File("resources/Pet.txt");
 
-	public Utility(Stage stage) throws FileNotFoundException, InterruptedException {
-		super(stage);
-
-	}
-
+	// Reads in file
 	public static boolean readFile() {
-		// Reads in file
-		File file = new File("Pet.txt");
+		/**
+		 * If the file is empty then it will open the intro screen so the user can
+		 * create a pet! if the file has something in it that means the user has created
+		 * a pet and will open the application
+		 **/
+
 		Scanner sc = null;
 
-		/**
-		 * If the file is empty then it will open the intro screen so the user can create a pet!
-		 * if the file has something in it that means the user has created a pet and will open the application
-		 **/
-		
 		try {
 			sc = new Scanner(file);
 			if (sc.hasNextLine() == true) {
@@ -39,8 +35,8 @@ public class Utility extends GameWindow {
 
 	}
 
+	// Writes in file
 	public static void save() {
-		// Writes in file
 		/**
 		 * Does not work because of protected image variable in pet class
 		 * ObjectOutputStream out = new ObjectOutputStream(new
@@ -51,16 +47,60 @@ public class Utility extends GameWindow {
 
 		try {
 
-			writer = new PrintWriter("Pet.txt");
+			writer = new PrintWriter(file);
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
 		// Writes in file to save information about the pet
-		writer.println(pet.getPetName() + "," + pet.getHealthLevel() + "," + pet.getTimeOfLastExcersize());
+		writer.println(
+				Main.pet.getPetName() + "," + Main.pet.getHealthLevel() + "," + Main.pet.getTimeOfLastExercise());
 		writer.close();
 
+	}
+
+	// Submission of pet name
+	public static void submission(Stage stage) {
+		save(); // Saves and sets variables
+
+		// Opens up main application after creating new pet
+		GameWindow instance;
+		try {
+			instance = new GameWindow(stage);
+			GameLoop loop = new GameLoop(instance);
+			loop.start();
+		} catch (FileNotFoundException | InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+	// Clears file
+	public static void clearPet() {
+		Scanner scnr = null;
+		PrintWriter writer = null;
+
+		try {
+			scnr = new Scanner(file);
+			writer = new PrintWriter(file);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		writer.print("");
+		Main.pet.equals(null);
+	}
+
+	// Loads in pet to create pet object
+	public static Pet loadPet() throws FileNotFoundException {
+		Scanner scnr = new Scanner(new File("resources/Pet.txt"));
+		String[] petString = scnr.next().split(",");
+		Corgi petToPass = new Corgi(petString[0], Integer.parseInt(petString[1]), Long.parseLong(petString[2]),
+				false /* TODO */);
+
+		return petToPass;
 	}
 
 }
